ec0bb81fafde26eac0e1f8a2c9ec1bf849829193
codorex <codorex.m@gmail.com>
2018-04-13 08:11:13 +0300

    Included Solution IntoToOOpCSharp

---
+++ CSharp OOP Basics/IntroToOOpCSharp/IntroCSharpOOP_2/Family.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCSharpOOP_2
+{
+    public class Family
+    {
+        private List<Person> members = new List<Person>();
+
+        public List<Person> Members
+        {
+            get
+            {
+                return members;
+            }
+            private set
+            {
+                members = value;
+            }
+        }
+
+        public void Add(Person member)
+        {
+            members.Add(member);
+        }
+
+        public void Exterminate(Person plebToExterminate)
+        {
+            members.Remove(plebToExterminate);
+        }
+    }
+}
